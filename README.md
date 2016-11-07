7aa13735c639cb6cbbe950f77ff819e788732a7f
Limon Monte <limonte@users.noreply.github.com>
2016-11-07 16:46:53 +0100

    Initial simple app version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# CSV Viewer Online
+# CSV Viewer Online [![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
 
 # [https://limonte.github.io/csv-viewer-online/](https://limonte.github.io/csv-viewer-online/)
---
+++ app.js
@@ -0,0 +1,26 @@
+/* global FileReader, Papa, Handsontable */
+
+var input = document.getElementById('input-file')
+var handsontableContainer = document.getElementById('handsontable-container')
+
+input.onchange = function () {
+  var file = this.files[0]
+  var reader = new FileReader()
+
+  reader.onload = function (e) {
+    var csv = e.target.result
+    var data = Papa.parse(csv, {
+      header: true,
+      skipEmptyLines: true
+    })
+
+    Handsontable(handsontableContainer, {
+      data: data.data,
+      rowHeaders: true,
+      colHeaders: data.meta.fields,
+      columnSorting: true
+    })
+  }
+
+  reader.readAsText(file)
+}
